35611404422d95c669401f9a2e80d157ee38b580
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-07-16 16:26:42 +0530

    created User entity

---
+++ src/main/java/com/demo/jwt_authentication/user/User.java
@@ -0,0 +1,27 @@
+package com.demo.jwt_authentication.user;
+
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="User")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
