81f2dd0bba2d9ca24fd481ddceada954fb683c5b
Sithira Bimsara <113254019+SBimsara@users.noreply.github.com>
2024-03-11 14:57:30 +0530

    implemented role based authorization using annotations

--- src/main/java/com/demo/jwt_authentication/config/SecurityConfig.java
+++ src/main/java/com/demo/jwt_authentication/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ import static com.demo.jwt_authentication.user.Role.MANAGER;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -52,11 +54,11 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PUT,"api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
                         .requestMatchers(HttpMethod.DELETE,"api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
 
-                        .requestMatchers("api/v1/admin/**").hasRole(ADMIN.name())
-                        .requestMatchers(HttpMethod.GET,"api/v1/admin/**").hasAuthority(ADMIN_READ.name())
-                        .requestMatchers(HttpMethod.POST,"api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
-                        .requestMatchers(HttpMethod.PUT,"api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
-                        .requestMatchers(HttpMethod.DELETE,"api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
+//                        .requestMatchers("api/v1/admin/**").hasRole(ADMIN.name())
+//                        .requestMatchers(HttpMethod.GET,"api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+//                        .requestMatchers(HttpMethod.POST,"api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+//                        .requestMatchers(HttpMethod.PUT,"api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
+//                        .requestMatchers(HttpMethod.DELETE,"api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
 
                         .anyRequest().authenticated()
                 )
--- src/main/java/com/demo/jwt_authentication/demo/AdminController.java
+++ src/main/java/com/demo/jwt_authentication/demo/AdminController.java
@@ -1,29 +1,35 @@
 package com.demo.jwt_authentication.demo;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/admin")
 @CrossOrigin(origins = "*")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     @GetMapping
+    @PreAuthorize("hasAuthority('admin:read')") //name of the permission is used
     public String get(){
         return "GET:: admin controller";
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('admin:create')")
     public String post(){
         return "POST:: admin controller";
     }
 
     @PutMapping
+    @PreAuthorize("hasAuthority('admin:update')")
     public String put(){
         return "PUT:: admin controller";
     }
 
     @DeleteMapping
+    @PreAuthorize("hasAuthority('admin:delete')")
     public String delete(){
         return "DELETE:: admin controller";
     }
